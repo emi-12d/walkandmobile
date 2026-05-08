faccca8875d04d285ea52a386266f9fd611d2f53
ZenZenZentaisei <c1007189@planet.kanazawa-it.ac.jp>
2026-05-08 17:42:14 +0900

    検証コードの削除

--- BrailleBlockRecognition_iOS/View/ViewController.swift
+++ BrailleBlockRecognition_iOS/View/ViewController.swift
@@ -434,7 +434,6 @@ extension ViewController: VideoCaptureDelegate {
         guidance.text = guideText
         // ある点字ブロックのキー作成
         let guidanceKey = code + angle + genre
-        print("ガイダンスキー" + guidanceKey)
         // 引数がSting型のためint型に変換
         let code = Int(code) ?? 0
         let angle = Int(angle) ?? -1
@@ -459,7 +458,6 @@ extension ViewController: VideoCaptureDelegate {
             // 案内文を取得
             let resultMessages = codeBlock.resultValue(key: guidanceKey, type: .guidance)
             let key = resultMessages.0
-            print("キー" + key)
             
             
             //データベースのキーと取得したキーを照合し、違ったら、ジャンルボタンを一般に変更
